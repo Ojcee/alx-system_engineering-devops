b11751010cb985b5e1dba01209f989654d57d4e2
Ottis Jennifer <105177827+Ojcee@users.noreply.github.com>
2023-06-14 00:38:10 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,6 @@
-file {'/etc/default/nginx' :
-    ensure => file,
-     content => "ULIMIT='-n 2050'\n",
+# Fixes an nginx site that can't handle multiple concurrent requests
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 2050\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
